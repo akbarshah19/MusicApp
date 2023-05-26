0f7d1af40cb170cf69b2d95f3dca57b4253cbb8c
Akbarshah <akbarshah19@icloud.com>
2023-05-26 23:51:33 +0500

    PlayerHeader added

---
+++ MusicApp/Views/PlayerHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerHeaderView.swift
+//  MusicApp
+//
+//  Created by Akbarshah Jumanazarov on 5/26/23.
+//
+
+import UIKit
+
+class PlayerHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
